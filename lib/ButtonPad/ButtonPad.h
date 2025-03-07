17e4accb4018971e6f2b36b446f31e867fa3039c
Egor <shulepov-e@abix.ru>
2025-03-07 10:54:01 +0300

    some comments and file structure reorganization

--- lib/ButtonPad/ButtonPad.h
+++ lib/ButtonPad/ButtonPad.h
@@ -14,7 +14,11 @@
 #define ROW3 5
 #define ROW4 4
 
+/// @brief initialize pin modes for the button pad
 void buttonStateInit();
+
+/// @brief writes the state of the buttons in bit form to the pointer
+/// @param state pointer to the variable where the state will be written in bit form
 void buttonStateUpdate(uint16_t *state);
 
 // #include "ButtonPad.cpp"
